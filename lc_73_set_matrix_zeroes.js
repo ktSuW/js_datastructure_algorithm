fd3bef45000dffc3bbd618afaa244438d037e8fa
Su <suwindev19@gmail.com>
2021-11-27 21:14:33 +1100

    Added 73.set matrix zeroes - used array to keep track of position, still O(m+n) space

--- lc_73_set_matrix_zeroes.js
+++ lc_73_set_matrix_zeroes.js
@@ -37,7 +37,34 @@ var setZeroes = function (matrix) {
   return matrix;
 };
 
+// Approach : array to keep track of 0 positions
+var setZeroes_1 = function (matrix) {
+  var track = [];
+
+  // find zeros
+  for (var i = 0; i < matrix.length; i++) {
+    for (var j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) track.push([i, j]);
+    }
+  }
+
+  for (var i = 0; i < track.length; i++) {
+    var [x, y] = track[i];
+
+    // update row
+    for (var j = 0; j < matrix[0].length; j++) {
+      matrix[x][j] = 0;
+    }
+
+    // udpate column
+    for (var j = 0; j < matrix.length; j++) {
+      matrix[j][y] = 0;
+    }
+  }
+  return matrix;
+};
+
 console.log("Creating 2D array");
 let twoDArray = new Array([0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]);
 
-console.log(setZeroes(twoDArray));
+console.log(setZeroes_1(twoDArray));
