0a6500ee1feeeb3add3a88f81fda06b30206ca88
Su <suwindev19@gmail.com>
2022-04-19 23:55:07 +1000

    Added : 70 -Top down with memoisation(array,map)

--- leetcode_solutions/lc_70_climbing_stairs.js
+++ leetcode_solutions/lc_70_climbing_stairs.js
@@ -1,5 +1,6 @@
 // Iterative Approach
-// Fibonacci Number
+// Fibonacci Number - DP with optimisation
+// Instead of using an array to store previous values, use previous two values 
 // Time complexity : O(n)
 // Space Complexity : O(1)
 var climbStairs = function (n) {
@@ -13,8 +14,7 @@ var climbStairs = function (n) {
   return twoSteps;
 };
 
-
-// Recursion Approach 
+// Recursion Approach
 /**
  * Brute force approach - Top down approach 
  * ====================================
@@ -37,3 +37,62 @@ var climbStairs = function (n) {
   if (n === 2) return 2;
   return climbStairs(n - 1) + climbStairs(n - 2);
 };
+
+// dynamic programming
+// bottom up approach
+var climbStairs = function (n) {
+  if (n <= 1) return 1;
+  // let dp = new Array(n+1);
+  let dp = [n + 1];
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+};
+
+
+// Using Map
+
+var climbStairs = function (n) {
+  let map = new Map();
+  map.set(1, 1);
+  map.set(2, 2);
+  let sum = 0;
+  for (let i = 3; i <= n; i++) {
+    sum = map.get(i - 1) + map.get(i - 2);
+    map.set(i, sum);
+  }
+  return map.get(n);
+};
+
+// recursive top down (memoization)
+var climbStairs = function (n) {
+  let memo = new Map();
+  return climb(n, memo);
+};
+
+var climb = function (n, memo) {
+  if (n <= 2) return n;
+  if (!memo.has(n)) {
+    memo.set(n, climb(n - 1, memo) + climb(n - 2, memo));
+  }
+  return memo.get(n);
+};
+
+// Top down memoisation 
+var climbStairs = function (n, memo) {
+  memo = memo || new Map();
+  if (memo.has(n)) return memo.get(n);
+
+  //Base cases
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+  let result = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+  memo.set(n, result);
+  return result;
+};
+
